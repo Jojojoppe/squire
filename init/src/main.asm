07906a9d4e5aa45aff655801f74930dc9c6c2540
Joppe Blondel <joppe@blondel.nl>
2020-07-18 20:22:38 +0200

    Created LOG syscall and removed debug interrupts
    Signed-off-by: Joppe Blondel <joppe@blondel.nl>

--- init/src/main.asm
+++ init/src/main.asm
@@ -10,22 +10,14 @@ bits 32
 section .data
 ; ------------
 S_00			db "This is init!", 0x0a, 0x0d, 0
-
-S_01			db "A", 0x0a, 0x0d, 0
-S_02			db "B", 0x0a, 0x0d, 0
-
 S_TESTBIN		db "testbin.bin", 0
-
-sc_message		dd	2			; PID
-				dd  0			; FLAGS
-				dd	18			; SIZE
-				db "Message from init", 0
 
 ; -----------
 ; SECTION BSS
 section .bss
 ; -----------
 
+sc_log			resd 2
 sc_process		resd 2
 
 pid_testbin		resd 1
@@ -47,8 +39,13 @@ _start:
 								; -12:	Length of testbin.bin
 		mov		[ebp-4], eax
 
-		mov		eax, S_00
-		int		0x81
+		; Print S_00
+		mov		edx, sc_log
+		mov		dword [edx+4*0], S_00
+		mov		dword [edx+4*1], 15
+		mov		ecx, 8
+		mov		eax, 0x10000000
+		int		0x80
 
 		; Get testbin.bin
 		mov		eax, [ebp-4]
@@ -71,12 +68,6 @@ _start:
 		mov		edx, sc_process
 		mov		eax, [edx+4*0]
 		mov		[pid_testbin], eax
-
-		; Message testbin
-		mov		edx, sc_message
-		mov		eax, 32
-		mov		ecx, 30	
-		int		0x80
 
 .lp0:
 		jmp		.lp0
