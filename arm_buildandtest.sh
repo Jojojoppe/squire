de53534f1feb3d3f6eb32d2f4ef39fb521fb875a
Joppe Blondel <joppe@blondel.nl>
2021-04-07 12:49:56 +0200

    Enabled ARM paging and jump to high half
    Signed-off-by: Joppe Blondel <joppe@blondel.nl>

--- arm_buildandtest.sh
+++ arm_buildandtest.sh
@@ -20,6 +20,7 @@ cd ..
 cd ..
 
 arm-none-eabi-objdump -d build/arm/output/boot/squire > DISASS_ARM
+arm-none-eabi-readelf -a build/arm/output/boot/squire >> DISASS_ARM
 
 echo STARTING QEMU
 qemu-system-arm -machine xilinx-zynq-a9 -kernel build/arm/output/boot/squire -serial /dev/null -serial mon:stdio -nographic -m 256M
